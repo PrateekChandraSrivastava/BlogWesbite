2a8edfe0aee96bfb7330ca46c20c773b6dbdc761
Prateek Chandra Srivastava <prateekchandrasrivastava@gmail.com>
2024-07-04 11:48:56 +0530

    Last commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 3000;
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
 
-app.use(express.static("Public"));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
